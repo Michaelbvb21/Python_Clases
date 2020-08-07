46cf1aa52d38eb396a0a07fc2440a9c4341582cb
Michaelbvb21 <bvbcarrasco21@gmail.com>
2020-08-07 14:44:47 -0500

    Agragando Ejercicios clase 2

---
+++ Clase 2/mayor_2_numeros.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  7 14:29:14 2020
+
+@author: Cristhoffer Carrasco
+"""
+
+"""
+Ejercicio1 condicionales
+"""
+
+def mayor(num1, num2 ):
+  if num1>num2:
+    resultado=print(f"El número mayor es {num1}")
+  elif num1<num2:
+    resultado=print(f"El número mayor es {num2}")
+  return resultado
+
+a=float(input('Ingrese el primer número: '))
+b=float(input('Ingrese el segundo número: '))
+
+mayor(a,b)
--- prueba.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug  5 14:35:09 2020
-
-@author: Cristhoffer Carrasco
-"""
-
-
-print("Hola nundo")
